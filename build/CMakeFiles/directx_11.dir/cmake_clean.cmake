499b9ef5899ddc6ac96070a662c4a03a5fbbcdd4
Fedmichard <francoisfedmichard@gmail.com>
2025-03-19 23:38:41 -0400

    Initial setup for DirectX 11 Project

---
+++ build/CMakeFiles/directx_11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directx_11.dir/src/main.cpp.obj"
+  "CMakeFiles/directx_11.dir/src/main.cpp.obj.d"
+  "directx_11.exe"
+  "directx_11.exe.manifest"
+  "directx_11.pdb"
+  "libdirectx_11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/directx_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
